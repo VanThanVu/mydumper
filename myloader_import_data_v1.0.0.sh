03ca770276545421319481fb473db5bbe74d1d29
Vu Van Than <46390756+VuVanThan-VN@users.noreply.github.com>
2020-10-28 16:51:08 +0700

    Create myloader_import_data_v1.0.0.sh

---
+++ myloader_import_data_v1.0.0.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dir_backup='/data/thanvv_secops/gossby'
+dir_logfile='/data/thanvv_secops/mydumper_import.log'
+db_user='root'
+db_pass='#####'
+db_hosts='x.x.x.x'
+time myloader --host=$db_hosts --user=$db_user --password=$db_pass --directory=$dir_backup --queries-per-transaction=50000 --threads=4 --verbose=3 -e 2>$dir_logfile
